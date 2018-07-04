5b5e78de6248e668e33de346864e2e4a1e5518ae
Lucien Brillant <lucienbrillant@gmail.com>
2018-07-04 14:18:52 -0700

    add docs to functions

--- src/functions.php
+++ src/functions.php
@@ -1,13 +1,27 @@
 <?php
 namespace Functional;
 
+/**
+ * Function composition
+ *
+ * @param callable $f1
+ * @param callable $f2
+ * @return callable
+ */
 function compose(callable $f1, callable $f2): callable {
   return static function (...$args) use ($f1, $f2) {
     return call_user_func($f1, call_user_func_array($f2, $args)); 
   };
 }
 
-function partial(callable $f, ...$args) {
+/**
+ * Partial application of a php callable.
+ *
+ * @param callable $f1
+ *
+ * @return callable
+ */
+function partial(callable $f, ...$args): callable {
   return static function(...$rest) use ($f, $args) {
     return call_user_func_array($f, array_merge($args, $rest));
   };
